8a722db9372d312ec139576c1b02b9893fdc129a
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-09-18 12:20:16 +0300

    enums

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 "use strict";
-const sum = (a, b) => {
-    let c;
-    return (c = a + b);
+// enums
+const four = () => {
+    return 2 + 2;
 };
-console.log(sum(10000, -5000));
+const allPersons = [
+    {
+        name: "Puga Admin",
+        status: 2 /* StatusEnum.ADMIN */,
+    },
+    {
+        name: "Puga user",
+        status: 3 /* StatusEnum.USER */,
+    },
+    {
+        name: "Puga Investor",
+        status: 4 /* StatusEnum.INVESTOR */,
+    },
+    {
+        name: "Puga Error",
+        status: 5 /* StatusEnum.ERROR */,
+    },
+];
+allPersons.map((person) => {
+    return console.log(`Name is: ${person.name}, and his role: ${person.status}`);
+});
--- app.ts
+++ app.ts
@@ -1,6 +1,34 @@
-const sum = (a:number, b:number):number => {
-  let c;
-  return (c = a + b);
-};
+// enums
 
-console.log(sum(10_000,-5_000))
+const four = (): number => {
+  return 2 + 2;
+};
+const enum StatusEnum {
+  ADMIN=2,
+  USER=3,
+  INVESTOR=4,
+  ERROR=5,
+}
+
+const allPersons = [
+  {
+    name: "Puga Admin",
+    status: StatusEnum.ADMIN,
+  },
+  {
+    name: "Puga user",
+    status: StatusEnum.USER,
+  },
+  {
+    name: "Puga Investor",
+    status: StatusEnum.INVESTOR,
+  },
+  {
+    name: "Puga Error",
+    status: StatusEnum.ERROR,
+  },
+];
+
+allPersons.map((person: { name: string; status: StatusEnum }) => {
+  return console.log(`Name is: ${person.name}, and his role: ${person.status}`);
+});
