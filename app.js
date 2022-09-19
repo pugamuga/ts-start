c2522036496e1e4373394cc0a9ab26e328380cf2
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-09-19 17:59:40 +0300

    generics

--- app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const multiplyArray = (array) => {
-    const myArrayOfNumbers = array.map((i) => Number(i))
-        .filter((i) => !Number.isNaN(i));
-    const multiply = myArrayOfNumbers.reduce((a, b) => a * b);
-    return console.log(multiply);
-};
-multiplyArray(["10", "15", "cat", 2]);
--- test.ts
+++ test.ts
@@ -1,30 +1,5 @@
-enum PaymentStatus {
-  Holded,
-  InProccess,
-  Rejected,
-}
+const myFunc = <T>(x: T): string => {
+  const newString = x.toString();
 
-class Payment {
-  id: number;
-  status: PaymentStatus;
-  createdAt: Date;
-  updatedAt!: Date;
-
-  constructor(id: number) {
-    this.id = id;
-    this.createdAt = new Date()
-    this.status = PaymentStatus.Holded
-  }
-
-  getPaymentLifeTime():number{
-    return new Date().getTime() - this.createdAt.getTime()
-  }
-
-  unholdPayment(){
-    if (this.status === PaymentStatus.InProccess){
-        throw new Error("Payments already in proccess")
-    }
-    this.status = PaymentStatus.Rejected
-    this.updatedAt = new Date()
-  }
-}
+  return newString;
+};
