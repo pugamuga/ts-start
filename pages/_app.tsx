94d1de9f8333e14e3353fb7963a64f6522c9d675
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-09-21 12:33:29 +0300

    create new

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
-import { AppProps } from 'next/dist/shared/lib/router/router'
-import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }:AppProps):JSX.Element {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,14 @@
+import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import testImage from "../assets/test.jpg";
-import NavBar from "../components/NavBar";
 
-export default function Home(): JSX.Element {
-  return (
-    <div className=" bg-gradient-to-tr from-violet-400 to-pink-500 w-screen h-[4000px] flex flex-col items-center">
-      <div className="fixed z-10 top-5 right-1/2 translate-x-1/2 ">
-        <NavBar/>
-      </div>
+const Home: NextPage = () => {
+  return <div>
+    <Head>
+      <title>Default Next Ts</title>
+      <link rel="icon" href="/favicon.ico"/>
+    </Head>
+  </div>;
+};
 
-      {[1, 2, 3, 4, 5, 6].map((i) => {
-        return (
-          <div className=" mix-blend-screen relative h-40 w-40 bg-white/50 rounded-full my-14 flex justify-center items-center text-5xl font-extrabold">
-            <h1 className="mb-2 text-black">{i}</h1>
-            <div className="h-12 w-12 absolute ring-8 ring-black rounded-full"></div>
-            <div className="h-20 w-20 absolute ring-[20px] ring-black rounded-md rotate-45 "></div>
-          </div>
-        );
-      })}
-      <div className="w-12 h-12" />
-    </div>
-  );
-}
+export default Home;
